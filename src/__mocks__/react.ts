d20ce0016d7d29a30495068481f0d0f758faf1d8
Andrii Lavreniuk <alo.andretti@gmail.com>
2026-05-17 13:43:10 +0200

    Support lazy mocked useState

--- src/__mocks__/react.ts
+++ src/__mocks__/react.ts
@@ -10,8 +10,9 @@ export function useReducer<S>(_: unknown, initialState: S) {
   return [initialState, () => undefined] as const;
 }
 
-export function useState<T>(value: T) {
-  return [value, () => undefined] as const;
+export function useState<T>(value: T | (() => T)) {
+  const initialValue = typeof value === "function" ? (value as () => T)() : value;
+  return [initialValue, () => undefined] as const;
 }
 
 export function useCallback<T>(callback: T) {
